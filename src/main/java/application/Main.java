716c02eecc4f534b4df7f941a810c7976c67bbe1
Jan Warnke <49824613+Eisteemarmelade@users.noreply.github.com>
2022-11-11 19:01:22 +0100

    added commentary

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -14,6 +14,8 @@ public class Main {
 
     public static void main( String[] args ) {
 
+        // bla
+
         Scanner scn = new Scanner( System.in );
 
         System.out.println( "Enter the multiplier for the items:" );
